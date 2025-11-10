7aac52f5944dd3800053e5a623015cc5b41d6006
pema <122863279+pemarsnv@users.noreply.github.com>
2025-11-10 16:37:27 +0100

    skeleton for diary and new account pages started

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ var audioClicking = new Audio('audios/bubble-pop.mp3');
 function connect() {
     var username = document.getElementById("log-user").value;
     var password = document.getElementById("log-password").value;
-    if ((username.localeCompare("admin") == 0) && (password.localeCompare("AlphonseLePlusBeauDeTous"))) {
-        console.log("caca");
-        audioClicking.play();
+    if ((username.localeCompare("admin") == 0) && (password.localeCompare("AlphonseLePlusBeauDeTous") == 0)) {
+        audioClicking.play().then(() => {window.location = "diary.html"});
     } else {
         console.log("pipi");
         document.getElementById("log-logs").style.visibility = "visible";
